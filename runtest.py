fada6c258c487cb2724ad8599f1eb8d11b017525
enrico <e.bison@tadaweb.com>
2021-05-04 08:19:46 +0200

    test reading pdf

--- runtest.py
+++ runtest.py
@@ -1,7 +1,22 @@
 import sys
+import os
+import PyPDF2
+import textract
 
 def test_pyprocsal():
     print(sys.argv)
+    files = os.listdir('local/')
+    print(files)
+    for f in files:
+        import textract
+        text = textract.process('local/{file}'.format(file=f))
+        print(text)
+
+        # pdfObj = open('local/{file}'.format(file=f), 'rb')
+        # pdfReader = PyPDF2.PdfFileReader(pdfObj)
+        # print(pdfReader.isEncrypted)
+        # pdfPage = pdfReader.getPage(0)
+        # print(pdfPage.extractText())
 
 if __name__ == "__main__":
     test_pyprocsal()
