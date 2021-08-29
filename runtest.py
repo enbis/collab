037d0e9d210436fa58e509b3fe6a8ba959e90f83
enrico <e.bison@tadaweb.com>
2021-08-30 00:03:06 +0200

    flask

--- runtest.py
+++ runtest.py
@@ -1,43 +1,20 @@
 import yaml
-import numpy as np
-from matplotlib import pyplot as plt
-from http.server import BaseHTTPRequestHandler, HTTPServer
 import logging
-import urllib.parse
+from flask import Flask, jsonify
 
 #read config as cfg
 with open("configs/config.yml", "r") as ymlfile:
     cfg = yaml.load(ymlfile, Loader=yaml.FullLoader)
+#flask application instance
+app = Flask(__name__)
 
-class Serve(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
-        self.end_headers()
-        self.wfile.write(b'Received')
-
-    def do_POST(self):
-        self.send_response(200)
-        self.end_headers()
-
-        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
-        post_body = self.rfile.read(content_length) # <--- Gets the data itself
-        #logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\nVersion:\n%s",
-        #        str(self.path), str(self.headers), post_data.decode('utf-8'), self.request_version)
-        logging.info("POST Body: \n%s\n", str(post_body.decode('utf-16')))
-        self.wfile.write("received post request:<br>{}".format(post_body))
-
-def run(server_class=HTTPServer, handler_class=Serve, port=9070):
-    server_address = (cfg["webserver"]["host"], cfg["webserver"]["port"])
-    httpd = server_class(server_address, handler_class)
-    logging.info('Starting httpd...\n')
-    try:
-        httpd.serve_forever()
-    except KeyboardInterrupt:
-        pass
-    httpd.server_close()
-    logging.info('Stopping httpd...\n')
+@app.route('/add')
+def value():
+    response = jsonify({'response': 'ok'})
+    response.status_code = 200
+    return response
 
 if __name__ == "__main__":
     logger = logging.basicConfig(level=logging.INFO)
     logging.info('Launching webserver...')
-    run()
+    app.run(host=cfg["webserver"]["host"], port=cfg["webserver"]["port"])
